4a231474a97e648c7676dc722068b5816aeea7e6
Guvener Gokce <guvener@guvenergokce.com>
2024-11-07 23:59:58 +0100

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,58 +1,65 @@
-const plugin = function(){
-    const makeZip = function (output, fileName = 'dist.zip') {
-        const path = require('path')
-        const fs = require('fs')
+const plugin = function() {
+    const makeZip = function(output, fileName = 'dist.zip') {
+        const path = require('path');
+        const fs = require('fs');
         const JSZip = require('jszip');
-        const zip = new JSZip()
-        const distPath = path.resolve(output)
-        const readDir = function (zip, dirPath) {
+        const zip = new JSZip();
+        const distPath = path.resolve(output);
+
+        const readDir = function(zip, dirPath) {
             const files = fs.readdirSync(dirPath);
             files.forEach(fileName => {
-                const fillPath = path.join(dirPath, "./", fileName)
+                const fillPath = path.join(dirPath, "./", fileName);
                 const file = fs.statSync(fillPath);
                 if (file.isDirectory()) {
                     const dirZip = zip.folder(fileName);
                     readDir(dirZip, fillPath);
                 } else {
-                    zip.file(fileName, fs.readFileSync(fillPath))
+                    zip.file(fileName, fs.readFileSync(fillPath));
                 }
             });
-        }
+        };
+
         const removeExistedZip = () => {
-            const dest = path.join(distPath, './' + fileName)
+            const dest = path.join(distPath, './' + fileName);
             if (fs.existsSync(dest)) {
-                fs.unlinkSync(dest)
+                fs.unlinkSync(dest);
             }
-        }
-        const zipDir = function () {
+        };
+
+        const zipDir = function() {
             readDir(zip, distPath);
             zip.generateAsync({
                 type: "nodebuffer",
                 compression: "DEFLATE",
-                compressionOptions: {
-                    level: 9
-                }
+                compressionOptions: { level: 9 }
             }).then(content => {
-                const dest = path.join(distPath, './' + fileName)
-                removeExistedZip()
+                const dest = path.join(distPath, './' + fileName);
+                removeExistedZip();
                 fs.writeFileSync(dest, content);
             });
-        }
-        removeExistedZip()
-        zipDir(distPath)
-    }
+        };
+
+        removeExistedZip();
+        zipDir(distPath);
+    };
+
     return {
         name: 'vite-plugin-uhuu',
-        apply: 'build',
-        closeBundle(){
-            const path = require('path')
-            const fs = require('fs')
-            const realPath = path.resolve(__dirname,'../../dist')
-            const name = 'uhuu.zip'
+        apply(config, { command, mode }) {
+            // Apply the plugin for all build commands with "build" in the mode
+            return command === 'build' || mode?.includes('build');
+        },
+        closeBundle() {
+            const path = require('path');
+            const fs = require('fs');
+            const realPath = path.resolve(__dirname, '../../dist');
+            const name = 'uhuu.zip';
             if (fs.existsSync(realPath)) {
-                makeZip(realPath,name)
+                makeZip(realPath, name);
             }
         }
-    }
-}
-module.exports = plugin
+    };
+};
+
+module.exports = plugin;
